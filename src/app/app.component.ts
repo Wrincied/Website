5737001cc5a1b345916ea9788148d8202bf483f4
HAINZ <63810038+Wrincied@users.noreply.github.com>
2021-03-27 11:56:40 +0300

    27.06 Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DragScrollComponent } from 'ngx-drag-scroll';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
 
-  public sliderItems: Array<{name: string, price: number}> = [
+  public sliderItemsMen: Array<{name: string, price: number}> = [
+    {
+      name: 'T-shirt',
+      price : 5,
+    },
+    {
+      name: 'Pants Forclaz',
+      price : 30,
+    },
+    {
+      name: 'Backpack',
+      price : 60,
+    },
+    {
+      name: 'Trainers',
+      price : 50,
+    },
+    {
+      name: 'Shirt',
+      price: 10,
+    },
     {
       name: 'T-shirt',
       price : 5,
@@ -31,10 +51,23 @@ export class AppComponent {
     }
   ];
 
-  constructor() {}
+  public sliderItemsWomen: Array<{name: string, price: number}> = [
+    
+  ]
+
+
+  @ViewChild('nav', { read: DragScrollComponent }) ds!: DragScrollComponent;
+
+  moveLeft() {
+    this.ds.moveLeft();
+  }
+
+  moveRight() {
+    this.ds.moveRight();
+  }
+  constructor() {
+  }
 
   ngOnInit() {
-
   }
 }
-  
